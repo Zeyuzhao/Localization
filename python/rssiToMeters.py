3c675a815355f5d4d023185f4fed2caea4cd3ed4
Nate Klein <nak314159@gmail.com>
2017-12-06 17:00:17 -0500

    Created linear calibration, added comment to conversion script

--- python/rssiToMeters.py
+++ python/rssiToMeters.py
@@ -4,4 +4,4 @@ def rssiToMeters(rssi, txPower):
     if (rssi == 0):
         return -1
     else:
-        return pow(10, (txPower - rssi) / 20)
+        return pow(10, (txPower - rssi) / 20)       # This formula will change based on the output of the calibration
