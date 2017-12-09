9b2d742b2870b4eac043a023696457962b2a5bbd
Nate Klein <nak314159@gmail.com>
2017-12-09 13:42:42 -0500

    Added bluepy use

--- python/linearCalibration.py
+++ python/linearCalibration.py
@@ -1,14 +1,28 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device", dev.addr)
+        elif isNewData:
+            print("Received new data from", dev.addr)
+
+scanner = Scanner().withDelegate(ScanDelegate())
+devices = scanner.scan(10.0)
+
 calibMap = {}
 NUM_POINTS = 10
 for i in range(NUM_POINTS):
     input("Move the pi " + str((i + 1)) + " meters away from the beacon, then hit Enter")
     rssiVals = []
     for j in range(25):
-        # Get RSSI value
-        # Add RSSI to list
-        pass
+        for dev in devices:
+            print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+            # for (adtype, desc, value) in dev.getScanData():
+            #     print("  %s = %s" % (desc, value))
     sum = 0
     for k in rssiVals:
         sum += k
