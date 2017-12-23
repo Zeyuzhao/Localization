aa683e25840ae5441840df8f417ed78ae9f061c4
Bryan Li <bryanli0@me.com>
2017-12-23 12:58:41 -0500

    Finished ols.

--- python/ols.py
+++ python/ols.py
@@ -5,9 +5,9 @@ def trilat(locs, radii):
     b = []
     l1 = locs.pop(0)
     r1 = radii.pop(0)
-    for dist, radius in zip(locs, radii):
-        A.append([dist[0] - l1[0], dist[1] - l1[1]])
-        b.append(r1 + (l1[0] - dist[0])**2 + (l1[1] - dist[1])**2 - radius)
+    for loc, radius in zip(locs, radii):
+        A.append([2*(loc[0] - l1[0]), 2*(loc[1] - l1[1])])
+        b.append(r1 + (loc[0] - l1[0])**2 + (loc[1] - l1[1])**2 - radius)
     matrixA = np.matrix(A)
     matrixB = np.matrix(b).reshape(3,1)
     deltaX = np.matrix(l1).reshape(2,1)
@@ -16,4 +16,6 @@ def trilat(locs, radii):
     X = Ainv * matrixB
     return (X + deltaX)
 
-print(trilat([[0,0],[10,0],[10,10],[10,10]],[58,84,50,25]))
+if __name__ == "__main__":
+    #Add test data here.
+    print(trilat([[3,3],[13,3],[3,13],[13,13]],[80,80,80,70]))
