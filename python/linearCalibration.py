7bda2aeac8be1e91af487d7a860c04a2642b613e
Nate Klein <nak314159@gmail.com>
2017-12-23 16:19:05 -0500

    Adjusted for snake

--- python/linearCalibration.py
+++ python/linearCalibration.py
@@ -16,22 +16,52 @@ VAL_NUM = 25
 
 calibMap = {}
 NUM_POINTS = 5
-for i in range(NUM_POINTS):
-    input("Move the pi " + str((i + 1)) + " meters away from the beacon, then hit Enter")
-    rssiVals = []
-    for j in range(VAL_NUM):
-        devices = scanner.scan(3.0)
-        for dev in devices:
-            if dev.addr == "b8:27:eb:c9:80:30":
-                rssiVals.append(dev.rssi)
-                print("RSSI = %ddB" % (dev.rssi))
-    rssiVals.sort()
-    rssiVals = rssiVals[int(VAL_NUM * .1):VAL_NUM - int(VAL_NUM * .1)]
-    sum = 0
-    for k in rssiVals:
-        sum += k
-    average = sum / len(rssiVals)
-    calibMap.update({i + 1 : average})
+def calib():
+    for i in range(NUM_POINTS):
+        input("Move the pi " + str((i + 1)) + " meters away from the beacon, then hit Enter")
+        rssiVals1 = []
+        rssiVals2 = []
+        rssiVals3 = []
+        rssiVals4 = []
+        for j in range(VAL_NUM):
+            devices = scanner.scan(3.0)
+            for dev in devices:
+                if dev.addr == "b8:27:eb:f4:45:e8":
+                    rssiVals1.append(dev.rssi)
+                    print("RSSI = %ddB" % (dev.rssi))
+                elif dev.addr == "b8:27:eb:45:95:14":
+                    rssiVals2.append(dev.rssi)
+                    print("RSSI = %ddB" % (dev.rssi))
+                elif dev.addr == "b8:27:eb:d7:4e:01":
+                    rssiVals3.append(dev.rssi)
+                    print("RSSI = %ddB" % (dev.rssi))
+                elif dev.addr == "b8:27:eb:c9:80:30":
+                    rssiVals2.append(dev.rssi)
+                    print("RSSI = %ddB" % (dev.rssi))
+        rssiVals1.sort()
+        rssiVals2.sort()
+        rssiVals3.sort()
+        rssiVals4.sort()
+        rssiVals1 = rssiVals1[int(VAL_NUM * .1):VAL_NUM - int(VAL_NUM * .1)]
+        rssiVals2 = rssiVals2[int(VAL_NUM * .1):VAL_NUM - int(VAL_NUM * .1)]
+        rssiVals3 = rssiVals3[int(VAL_NUM * .1):VAL_NUM - int(VAL_NUM * .1)]
+        rssiVals4 = rssiVals4[int(VAL_NUM * .1):VAL_NUM - int(VAL_NUM * .1)]
+        sum = 0
+        for k in rssiVals1:
+            sum += k
+        average1 = sum / len(rssiVals1)
+        for k in rssiVals2:
+            sum += k
+        average2 = sum / len(rssiVals2)
+        for k in rssiVals3:
+            sum += k
+        average3 = sum / len(rssiVals3)
+        for k in rssiVals4:
+            sum += k
+        average4 = sum / len(rssiVals4)
+        allAverage = (average1, average2, average3, average4)
+        calibMap.update({i + 1 : allAverage})
+        return allAverage
 
 # Write to file to run regression in excel
 f = open("calibFile.csv", "w+")
