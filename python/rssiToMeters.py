4cc88968a68eec11a49535ee0ceee35065636ca8
Nate Klein <nak314159@gmail.com>
2017-12-23 15:49:39 -0500

    Final rssi to m formula

--- python/rssiToMeters.py
+++ python/rssiToMeters.py
@@ -4,6 +4,6 @@ def rssiToMeters(rssi):
     if (rssi == 0):
         return -1
     else:
-        return (1.04224 * pow(10, -13)) * pow((rssi * -1), 7.73395)
+        return pow(0.966559, rssi) -3.4515
 
 print(rssiToMeters(-48.68))
