df0b7b51257a7d69b54c4254a96f3c35d377cb26
Zach Zhao <Zachzhao@outlook.com>
2017-12-25 16:08:38 -0500

    Some additional Reorganization changes

--- python/rssi.py
+++ python/rssi.py
@@ -3,7 +3,7 @@
 import pickle
 import os
 
-from linearCalibration import calib
+
 
 NUM_SAMPLES = 1
 NUM_BEACON = 4
@@ -135,22 +135,8 @@ def calib(x , y):
 '''
 
 if __name__ == "__main__":
-    x_length = 4
-    y_length = 3
-    spacing = 1
-    grid = RssiGrid(NUM_BEACON, x_length, y_length, spacing)
-
-    while(not grid.isComplete()):
-        rssiTuple = calib()
-        for i in range(NUM_BEACON):
-            print("Beacon: {}, Value: {}, Coord: ({}, {})".format(i, rssiTuple[i], grid.currentI, grid.currentJ))
-            grid.addRssi(i, rssiTuple[i])
-        input("Press Enter to Continue...")
-        f = open('grid.pckl', 'wb')
-        pickle.dump(grid, f)
-        f.close()
-    #print2D(grid)
-
+    with open('grid.pckl', 'rb') as f:
+        grid = pickle.load(f)
 
 
 
